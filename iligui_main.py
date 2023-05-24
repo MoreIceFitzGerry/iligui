5256400be4a8c5b72919a991fe0767fc97b90f2b
MoreIceFitzGerry <maurice@fitzgerald.ch>
2023-05-24 15:58:41 +0200

    complete commit Before another big change

--- iligui_main.py
+++ iligui_main.py
@@ -1,8 +1,13 @@
 from PyQt6.QtWidgets import QMainWindow, QApplication, QFileDialog
-from PyQt6.QtWidgets import QLabel, QToolButton, QPlainTextEdit,QProgressBar, QToolBox
+from PyQt6.QtWidgets import QLabel, QToolButton, QPlainTextEdit, QProgressBar, QToolBox
+from PyQt6.QtCore import QPropertyAnimation, QSize, QRect, QVariantAnimation
 from PyQt6 import uic, QtGui
 from iligui_settings import ilisettingsgui
 import sys
+import logic_playbutton
+import xml.etree.ElementTree as ET
+import webbrowser
+import time
 
 
 class iligui(QMainWindow):
@@ -11,21 +16,26 @@ class iligui(QMainWindow):
 
         # Load UI File
         uic.loadUi("window_main.ui", self)
+        # Set the Icon -> do this with Pyinstaller!
+        #self.setWindowIcon(QtGui.QIcon("icons/interlis.png"))
 
         # Define our Widgets----------------------------------------------------------------------------------------------
+        ## Central Widget
+        self.centralwidget
+
         ## Main Widgets
         self.interlisButton
         self.settingsButton
         self.fileselectButton
         self.modelselectButton
         self.playButton
+        self.topFrame
         self.errorFrame
-        self.errorFrame.setVisible(False)
+        self.errorFrame.setVisible(False) # hide frame
         self.errorText
-        self.errorText.setReadOnly(True)
         self.errorButton
         self.helpFrame
-        self.helpFrame.setVisible(False)
+        self.helpFrame.setVisible(False) # hide frame
         self.helpText
         self.helpButton
 
@@ -60,7 +70,8 @@ class iligui(QMainWindow):
         self.fileselectButton.clicked.connect(self.fileselect)
         self.modelselectButton.clicked.connect(self.modelselect)
         self.playButton.clicked.connect(self.playselect)
-        self.errorButton.clicked.connect(self.errorselect)
+        self.errorButton.clicked.connect(self.helptoggle)
+        self.helpButton.clicked.connect(self.furtherhelp)
 
         # Ensure Readiness for Java Interoperability----------------------------------------------------------------------
         # Check JRE Installation in Current working directory, if not install in CWD
@@ -70,10 +81,9 @@ class iligui(QMainWindow):
 
         # Show the App directly at the end of initialization
         self.show()
-        self.myWidth = self.width()
-        self.myHeight = self.minimumHeight()
-        self.resize(self.myWidth, self.myHeight)
-
+        # self.resize(self.width(), self.topFrame.sizeHint().height())
+        # self.centralwidget.adjustSize() # Adjusts the central widget to fit its contents minimally
+        self.adjustSize() # Adjusts the main window to fit its contents minimally
 
     # Methods to Run TODO: SEPERATE FROM THIS FILE AND IMPORT-------------------------------------------------------------
     """
@@ -99,32 +109,133 @@ class iligui(QMainWindow):
         print("Loaded Model from " + self.model_path)
 
     def playselect(self):
-        # Use subprocess method to run the Jar file with the selected input options
-        import logic_playbutton
         try:
-            textoutput = logic_playbutton.run_ilivalidator(self.settings, self.file_path)
-            if "Info: ...validation done" in textoutput:
-                self.errorFrame.setVisible(False)
-                self.helpFrame.setVisible(False)
-                self.playButton.setIcon(QtGui.QIcon("icons/circle_good_green.png"))
-                self.resize(self.myWidth, self.myHeight)
+            # Use subprocess method to run the Jar file with the selected input options
+            xtflog_path = logic_playbutton.run_ilivalidator(self.settings, self.file_path)
+            # Prepare the XTF File for element finding
+            tree = ET.parse(xtflog_path)
+            root = tree.getroot()
+            # Get the namespace of the root element
+            ns = root.tag.split('}')[0].strip('{')
+
+            # Find Elements of the IliVErrors.Errorlog where <Type> has text "Error"
+            error_elements = []
+            for element in root.findall(".//{%s}IliVErrors.ErrorLog.Error" % ns):
+                element_type = element.find("{%s}Type" % ns).text
+                if element_type == "Error":
+                    errorMessage = element.find("{%s}Message" % ns)
+                    DataSource = element.find("{%s}DataSource" % ns)
+                    # Then find the following elements, IF THEY EXIST (Can vary depending on the error)
+                    Line = element.find("{%s}Line" % ns)
+                    objectTag = element.find("{%s}ObjTag" % ns)
+                    tagIdentification = element.find("{%s}Tid" % ns)
+
+                    if objectTag is None or tagIdentification is None or Line is None:
+                        # print("errorMessage: ", errorMessage.text)
+                        error_elements.append(f"<a href='file:///{self.file_path}' style='color:red;'>Error found: {errorMessage.text}</a>")
+                    else:
+                        # print("errorMessage: ", errorMessage.text, "objectTag: ", objectTag.text, "tagIdentification: ", tagIdentification.text, "DataSource: ", DataSource.text, "Line: ", Line.text)
+                        error_elements.append(f"<a href='file:///{self.file_path}' style='color:red;'>Error found: Line {Line.text}, TagID {tagIdentification.text}, objectTag {objectTag.text}: {errorMessage.text}</a>")
+
+            # Preload the Help text for the Found Errors
+            """
+            import errorMessages
+            for error_element in error_elements:
+                for errorMessage in errorMessages:
+                    if error_element in errorMessage:
+                        print(errorMessage)
+            """
+            # Convert the error_elements list to a string for printing
+            print(error_elements)
+            if (len(error_elements) == 1):
+                error_text = error_elements[0]
             else:
+                error_text = "<br>".join(error_elements) # Join them with breaks
+            print(error_text)
+
+            with open(xtflog_path, 'r') as f:
+                xtflog_content = f.read()
+                # Parse the xtf log file here and extract the relevant information
+
+            if "...validation done" in xtflog_content:
+                self.playButton.setIcon(QtGui.QIcon("icons/circle_good_green.png"))
+                self.hideErrorFrame()
+            else:
+                print("Validation Error...")
                 self.playButton.setIcon(QtGui.QIcon("icons/circle_bad_red.png"))
-                self.errorFrame.setVisible(True)
-                self.errorText.appendPlainText(textoutput)
+                self.showErrorFrame()
+                self.errorText.setText(error_text)
 
         except AttributeError as e:
-            self.errorFrame.setVisible(True)
-            self.errorText.appendPlainText(repr(e))
+            print("Execution Error...")
             self.playButton.setIcon(QtGui.QIcon("icons/circle_bad_red.png"))
-            self.errorText.appendPlainText("An XML Transfer-File must be selected!")
+            self.showErrorFrame()
+            self.errorText.setText(f"<a style='color:red;'>Error found: {repr(e)}</a>")
 
-    def errorselect(self):
-        self.helpFrame.setVisible(True)
-        self.helpText.appendPlainText("Some Help Suggestions")
+    # Resize-Animation of MainWindow Height
+    def updateHeight(self, value):
+        self.resize(self.width(), value)
+    def resizeMainWindowHeight(self, oldHeight, newHeight):
+        self.resizeAnimation = QVariantAnimation()
+        self.resizeAnimation.setDuration(300)
+        self.resizeAnimation.setStartValue(oldHeight)
+        self.resizeAnimation.setEndValue(newHeight)
+        self.resizeAnimation.valueChanged.connect(self.updateHeight) # Update the main window's height with current value of the animation
+        self.resizeAnimation.start()
+
+    def mainWindowHeightShrink(self):
+        oldHeight = self.height() # Get current height
+        newHeight = self.height() - self.errorFrame.sizeHint().height() # Get the recommended height from sizeHint
+
+    def showErrorFrame(self):
+        # Regrow the frame to its recommended size
+        # newHeight = self.errorFrame.sizeHint().height() # Get recommended height
+        self.errorFrame.setVisible(True) # Show the Frame
+        # newHeight = self.errorFrame.sizeHint().height()
+        # self.errorFrame.resize(self.width(), newHeight)
+        
+        oldHeight = self.height() # Get current height
+        newHeight = self.sizeHint().height() # Get the recommended height from sizeHint
+        self.mainWindowHeightGrow()
+
+    def hideErrorFrame(self):
+        self.errorFrame.setVisible(False) # Hide the Frame
+        self.mainWindowHeightShrink()
+        # if self.errorFrame.height() != 1: # Skip animation if frame is already shrunk
+            # Shrink the frame to minimal height
+            # oldHeight = self.errorFrame.height() # Get current height
+            # newHeight = 1
+            # self.animation = QVariantAnimation()
+            # self.animation.setDuration(300)
+            # self.animation.setStartValue(oldHeight)
+            # self.animation.setEndValue(newHeight)
+            # self.animation.valueChanged.connect(self.updateErrorFrameHeight)
+            # self.animation.finished.connect(self.mainWindowHeightResize)
+            # self.animation.start()
+        # else:
+            # pass
+
+    # Resize-Animations of helpFrame Height
+    def updateHelpFrameHeight(self, value):
+        self.helpFrame.resize(self.width(), value)
+    def helptoggle(self):
+        if self.helpFrame.height() == 1: # If frame is already shrunk, grow animation
+            self.helpFrame.setVisible(True) # Show the Frame
+            newHeight = self.helpFrame.sizeHint().height()
+            self.helpFrame.resize(self.width(), newHeight)
+            self.mainWindowHeightResize()
+        else:
+            # Shrink the frame to minimal height
+            newHeight = 1
+            self.helpFrame.resize(self.width(), newHeight)
+            self.helpFrame.setVisible(False)# Hide the Frame
+            self.mainWindowHeightResize()
+
+    def furtherhelp(self):
+        # TODO: Check for validity of URL
+        webbrowser.open("https://geostandards-ch.github.io/doc_refhb24/")
 
     def interlisselect(self):
-        import webbrowser
         # TODO: Check for validity of URL
         webbrowser.open("https://github.com/claeis/ilivalidator/blob/master/docs/ilivalidator.rst")
 
@@ -140,3 +251,6 @@ class iligui(QMainWindow):
 app = QApplication(sys.argv)
 UIWindow = iligui()
 app.exec()
+
+# self.errorFrame.setFixedHeight(self.errorFrame.sizeHint().height()) # show frame
+# self.errorFrame.setFixedHeight(0) # hide frame
--- logic_playbutton.py
+++ logic_playbutton.py
@@ -1,11 +1,11 @@
 from PyQt6.QtCore import QProcess
 
 def run_ilivalidator(settings_list, file):
-    #print("Command: " + str(Options) + " " + file)
     # Aufruf-Syntax = java -jar ilivalidator.jar [Options] [file]
     java_path = "java11\\bin\\java.exe"
     ilivalidator_path = "ilivalidator-1.13.2\\ilivalidator-1.13.2.jar" # .jar file path
-    settings_list.append("--xtflog result.xtf") # in any case we want the xtflog
+    xtflog_path = "result.xtf"
+    settings_list.append(f"--xtflog {xtflog_path}") # in any case we want the xtflog
     option_string = " ".join(str(option) for option in settings_list)
     print(f"Option_string: {option_string}")
 
@@ -21,12 +21,13 @@ def run_ilivalidator(settings_list, file):
     # setProcessChannelMode(QProcess::MergedChannels) will merge the output channels.
     # Various programs write to different outputs.
     # Some use the error output for their normal logging, some use the "standard" output, some both. Better to merge them.
-    output= process.readAllStandardOutput()
+    ####output= process.readAllStandardOutput()
     #error = process.readAllStandardError()
 
     # The programs output is in byte and needs to be decoded with the standard utf8.
     # It's possible some characters may have been output by the program that don't conform,
     # so we ingore these errors and simply pass what can be.
-    textoutput = bytes(output).decode("utf8", "ignore")
-    print(textoutput)
-    return textoutput
+    ####textoutput = bytes(output).decode("utf8", "ignore")
+    #####print(textoutput)
+    #####return textoutput
+    return xtflog_path
